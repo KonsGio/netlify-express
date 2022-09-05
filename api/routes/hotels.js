b4e655d152a5e3b43e683956207c95c26d9c16ec
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-09-06 02:11:56 +0300

    ADDED - Update, delete hotel (specifix id)

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -8,15 +8,37 @@ router.post('/', async (req,res) => {
     const newHotel = new Hotel(req.body)
 
     try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
     }catch(err){
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
 })
+
 // UPDATE
+router.put('/:id', async (req,res) => {
+
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatedHotel);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 // DELETE
+router.delete('/:id', async (req,res) => {
+
+    try{
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted");
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 // GET
+
 // GET ALL
 
 export default router
