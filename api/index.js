ce86740e8f0c21960707611dc91bf18608656e62
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-09-28 18:53:49 +0300

    Error handling fixed for error messages to appear

--- api/index.js
+++ api/index.js
@@ -36,16 +36,16 @@ const connect = async () => {
   app.use("/api/hotels", hotelsRoute);
   app.use("/api/rooms", roomsRoute);
 
-  // // another middleware 
-  
-  // app.use((req,res,next) => {
-  //   console.log('I am a middleware');
-  // })
-
   // Error handling
-    app.use((err,req,res,next) => {
-    return res.status(500).json("Error from handler")
-  })
+  app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+    });
+  });
 
 app.listen(8800, () => {
     connect()
