def4aa2d922630186ab9bfd000af7afd61f75bda
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-10-05 22:51:30 +0300

    Verify user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -10,9 +10,8 @@ export const register = async (req, res, next) => {
         const hash = bcrypt.hashSync(req.body.password,salt);
 
         const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash,
+          ...req.body,
+          password: hash,
         })
 
         await newUser.save()
@@ -37,7 +36,7 @@ export const login = async (req, res, next) => {
         const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, process.env.JWT);
 
         const {password, isAdmin, ...otherDetails} = user._doc;
-        res.cookie("access token", token, {
+        res.cookie("access_token", token, {
           httpOnly: true,
         }).status(200).json({...otherDetails});
     } catch (err) {
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
-import {verifyToken } from "../utils/verifyToken.js";
+import {verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
@@ -8,9 +8,9 @@ router.get("/checkauthentication", verifyToken, (req,res,next)=>{
   res.send("hello user, you are logged in")
 })
 
-// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
-//   res.send("hello user, you are logged in and you can delete your account")
-// })
+router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+  res.send("hello user, you are logged in and you can delete your account")
+})
 
 // router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
 //   res.send("hello admin, you are logged in and you can delete all accounts")
